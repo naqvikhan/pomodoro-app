51a70273c6eb01784dc9b77ba906c7f55e8b6b88
M Naqvi Khan <naqvikhna98@gmail.com>
2023-07-29 15:24:33 -0500

    theme update
    updating theme on the app

--- src/Timer.js
+++ src/Timer.js
@@ -4,29 +4,43 @@ import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
 
-const red = '#f54e4e';
-const green = '#4aec8c';
+const cream = '#E6E3D3';
+// const green = '#4aec8c';
 
 function Timer(){
     return(
-        <div>
-            <CircularProgressbar strokeWidth={10} value={60} text={`60%`} styles={buildStyles({
-            strokeLinecap: 15, 
-            rotation: 270,
-            textColor:'#fff', 
-            pathColor:red, 
-            tailColor:'rgba(255,255,255,.2)',
-            })} /> 
         
-            <div style={{marginTop:'20px'}}>
-                <PlayButton />
-                <PauseButton />
-            </div>
-            <div style={{marginTop:'20px'}}>
-                <SettingsButton/>
-            </div>
             
-        </div>
+        <>
+        <text className='app-title topleft'>
+            Pomodoro.
+        </text>
+        
+        
+        <div>
+                
+                <CircularProgressbar strokeWidth={6} value={60} text={`60%`}  styles={buildStyles({ 
+                    rotation: 15, 
+                    strokeLinecap: 0,
+                    textColor:'#fff', 
+                    pathColor:cream, 
+                    backgroundColor: 'rgba(255,255,255,.2)',
+                    trailColor:'rgba(255,255,255,.2)',
+                })} /> 
+            
+                <div style={{marginTop:'20px'}}>
+                    <PlayButton />
+                    <PauseButton />
+                    
+                </div>
+    
+                <div style={{marginTop:'20px'}}>
+                    <SettingsButton/>
+                </div>
+                
+            </div>
+        </>
+            
     );
         
 }
