3ce3cadd4b7ee7611d67b4226b08d0f77d342c1b
M Naqvi Khan <naqvikhna98@gmail.com>
2023-08-04 20:17:15 -0500

    settings screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Timer from './Timer';
+import Settings from './Settings';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  // eslint-disable-next-line
+  const [showSettings, setShowSettings] = useState(true);
+
   return (
     <main>
-      <Timer/>
+      {showSettings ? <Settings /> : <Timer/>}
     </main>
   );
 }
---
+++ src/Settings.js
@@ -0,0 +1,28 @@
+import ReactSlider from 'react-slider';
+import './Slider.css';
+
+function Settings(){
+    return(
+        <div style={{textAlign:'left'}}>
+            <label>Work Minutes:</label>
+            <ReactSlider
+                className={'slider'}
+                thumbClassName={'thumb'}
+                trackClassName={'track'}
+                value={45}
+                min={1}
+                max={120}
+            />
+            <label>Break Minutes:</label>
+            <ReactSlider
+                className={'slider'}
+                thumbClassName={'thumb'}
+                trackClassName={'track'}
+                value={45}
+                min={1}
+                max={120}
+            />
+        </div>
+    );
+}
+export default Settings;
